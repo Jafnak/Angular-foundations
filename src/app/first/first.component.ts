4473612e73ebf01877998d6db6e234bf8ed8518c
Jafna K <jafnakusman@gmail.com>
2026-03-17 12:43:14 +0530

    calculator

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent {
+  name1=""
+  fun(){
+    alert (this.name1)
+  }
 
+  num1:number=0;
+  num2:number=0;
+  result:number=0;
+
+  addition(){
+    this.result=this.num1+this.num2
+  }
+
+  substraction(){
+    this.result=this.num1-this.num2
+  }
+
+  multiplication(){
+    this.result=this.num1*this.num2
+  }
+
+  division(){
+    this.result=this.num1/this.num2
+  }
 }
